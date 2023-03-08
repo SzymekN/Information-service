d4b2642da6cd1c1190312afa154c2e1575e7c7ce
Szymon Nowak <83112762+SzymekN@users.noreply.github.com>
2023-03-08 18:08:36 +0100

    quill text editor v1

---
+++ website/src/components/Editor.vue
@@ -0,0 +1,24 @@
+<script>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+export default {
+  components: {
+    QuillEditor
+  }
+}
+</script>
+
+<template>
+    <div>
+        <QuillEditor theme="snow" toolbar="full"/>
+    </div>
+</template>
+
+
+<style scoped>
+ div {
+    width: 100%;
+    height: 100vh;
+ }
+</style>
--- website/src/main.js
+++ website/src/main.js
@@ -6,6 +6,7 @@ import './assets/main.css'
 
 const app = createApp(App)
 
+
 app.use(router)
 
 app.mount('#app')
