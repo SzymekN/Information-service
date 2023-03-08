a9d04f9c6bede97bc8aced332798771aa4535a7e
Szymon Nowak <83112762+SzymekN@users.noreply.github.com>
2023-03-08 16:30:23 +0100

    front page + reorder

---
+++ website/src/components/TheNavBar.vue
@@ -0,0 +1,35 @@
+<script setup>
+import list from "../data/nav.json"
+</script>
+
+<template>
+  <table>
+    <div class="buttons" v-for="item in list">
+      <button><router-link :to="item.link" ><a class="aButton">{{item.text}}</a></router-link></button>
+    </div>
+  </table>
+
+ 
+</template>
+
+
+<style scoped>
+
+.buttons {
+  float: left;
+  border-top: 0.1rem solid #8f8f8f;
+  align-content: center;
+}
+
+.buttons button {
+  background: linear-gradient(aliceblue, #ffffff);
+  border: none;
+  padding: 1rem;
+  font-size: 1rem;
+}
+
+.buttons button:hover {
+  background: linear-gradient(#d6e7ef, #ffffff);
+  /*border-bottom: 1px solid #8f8f8f;*/
+}
+</style>
---
+++ website/src/components/Weather.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+ import CurrentWeather from './CurrentWeather.vue';
+ import Forecast from './Forecast.vue';
+</script>
+
+<template>
+<div>
+    <CurrentWeather></CurrentWeather>
+    <hr>
+    <Forecast></Forecast>
+</div>
+</template>
