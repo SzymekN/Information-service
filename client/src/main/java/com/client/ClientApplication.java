e7cc3becc45e051f835e628ad042ac92d5ed01b8
xDekann <oozog255@gmail.com>
2023-04-06 22:55:57 +0200

    Example of cookie with role implement + typo fix
    When user logs in (successfully) he is being redirected to "/client/test" endpoint. Here he receives cookie with his role name. This cookie might have a certain lifespan, after each login it should be updated. After log out (accessing /client/logout) cookie is being replaced by a cookie with lifespan 0 and null content (deleted)

--- client/src/main/java/com/client/ClientApplication.java
+++ client/src/main/java/com/client/ClientApplication.java
@@ -1,5 +1,6 @@
 package com.client;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Base64;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 60)
@@ -26,7 +31,7 @@ public class ClientApplication {
 	private String instanceId;
 
 	@GetMapping("/login")
-	public String idTest(){
+	public String idTest() {;
 		return "login";
 	}
 
@@ -34,17 +39,26 @@ public class ClientApplication {
 	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		if (auth != null){
-			new SecurityContextLogoutHandler().logout(request, response, auth);
-			//request.getSession().invalidate();
+			new SecurityContextLogoutHandler().logout(request, response, auth);;
+			Cookie cookie = new Cookie("ROLE", null);
+			cookie.setPath("/");
+			cookie.setMaxAge(0);
+			response.addCookie(cookie);
 			System.out.println("IN LOGOUT - delete handler");
-			//SecurityContextHolder.clearContext();
 		}
 		return "redirect:/client/login?logout";
 	}
 
 	@GetMapping("/test")
-	public String testIdAfterLogin(Model model){
+	public String testIdAfterLogin(Model model, HttpServletRequest request, HttpServletResponse response){
+		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String role = userDetails.getAuthorities().iterator().next().getAuthority();
+		Cookie cookie = new Cookie("ROLE", Base64.getEncoder().encodeToString(role.getBytes()));
+		cookie.setPath("/");
+		cookie.setMaxAge(3600 * 24); // seconds provided (1 day)
+		response.addCookie(cookie);
 		model.addAttribute("instanceId", "ID: " + instanceId);
+		System.out.println("Logged and redirected - getting cookie: " + cookie);
 		return "test-id";
 	}
 
