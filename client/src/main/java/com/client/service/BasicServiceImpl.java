5534d7fd723b67bb0d3a04ebe9e4ad7e661b7bbd
xDekann <oozog255@gmail.com>
2023-04-10 17:03:23 +0200

    Final variables name cleaning

--- client/src/main/java/com/client/service/BasicServiceImpl.java
+++ client/src/main/java/com/client/service/BasicServiceImpl.java
@@ -12,27 +12,27 @@ import java.util.Base64;
 public class BasicServiceImpl {
 
     public void roleCookieCreation(HttpServletRequest request, HttpServletResponse response) {
-        final int cookieDuration = 3600 * 24;
-        final String cookiePath = "/";
-        final String cookieName = "ROLE";
+        final int COOKIE_DURATION = 3600 * 24;
+        final String COOKIE_PATH = "/";
+        final String COOKIE_NAME = "ROLE";
 
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String role = userDetails.getAuthorities().iterator().next().getAuthority();
-        Cookie cookie = new Cookie(cookieName, Base64.getEncoder().encodeToString(role.getBytes()));
-        cookie.setPath(cookiePath);
-        cookie.setMaxAge(cookieDuration); // seconds provided (1 day)
+        Cookie cookie = new Cookie(COOKIE_NAME, Base64.getEncoder().encodeToString(role.getBytes()));
+        cookie.setPath(COOKIE_PATH);
+        cookie.setMaxAge(COOKIE_DURATION); // seconds provided (1 day)
         response.addCookie(cookie);
         System.out.println("Logged and redirected - getting cookie: " + cookie);
     }
 
     public void roleCookieRemoval(HttpServletRequest request, HttpServletResponse response) {
-        final int cookieDuration = 0;
-        final String cookiePath = "/";
-        final String cookieName = "ROLE";
+        final int COOKIE_DURATION = 0;
+        final String COOKIE_PATH = "/";
+        final String COOKIE_NAME = "ROLE";
 
-        Cookie cookie = new Cookie(cookieName, null);
-        cookie.setPath(cookiePath);
-        cookie.setMaxAge(cookieDuration);
+        Cookie cookie = new Cookie(COOKIE_NAME, null);
+        cookie.setPath(COOKIE_PATH);
+        cookie.setMaxAge(COOKIE_DURATION);
         response.addCookie(cookie);
     }
 }
