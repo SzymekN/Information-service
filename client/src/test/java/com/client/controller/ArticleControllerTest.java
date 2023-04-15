8e48b030c91843392577be508546f1ac7ab0fcdf
xDekann <oozog255@gmail.com>
2023-04-15 22:11:18 +0200

    Paginated endpoints + initial backend readme
    Added paginated endpoints for client side articles that support both all articles and by category pagination. Also added db and integration tests for them and initial readme for these endpoints.

--- client/src/test/java/com/client/controller/ArticleControllerTest.java
+++ client/src/test/java/com/client/controller/ArticleControllerTest.java
@@ -3,6 +3,7 @@ package com.client.controller;
 import com.client.model.dto.ArticleDto;
 import com.client.security.SecurityConfig;
 import com.client.service.ArticleService;
+import com.client.service.BasicServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -38,6 +39,8 @@ public class ArticleControllerTest {
     private MockMvc mockMvc;
     @MockBean
     private ArticleService articleService;
+    @MockBean
+    private BasicServiceImpl basicServiceImpl;
 
     private List<ArticleDto> articleDtoList;
 
@@ -74,14 +77,16 @@ public class ArticleControllerTest {
     @Test
     @WithAnonymousUser
     public void get_all_articles_with_param() throws Exception {
-        // given & when
+        // given
+        String category = "politics";
+        // when
         List<ArticleDto> articleDtosEdited = articleDtoList;
         articleDtosEdited.remove(1);
-        when(articleService.findByCategory("politics")).thenReturn(articleDtosEdited);
-        String expected = new ObjectMapper().writeValueAsString(articleService.findByCategory("politics"));
+        when(articleService.findByCategory(category)).thenReturn(articleDtosEdited);
+        String expected = new ObjectMapper().writeValueAsString(articleService.findByCategory(category));
         // then
         mockMvc.perform(MockMvcRequestBuilders.get("/client/articles")
-                        .param("category","politics"))
+                        .param("category",category))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(result -> JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false));
@@ -95,5 +100,62 @@ public class ArticleControllerTest {
         // then
         mockMvc.perform(MockMvcRequestBuilders.get("/client/articles"))
                 .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void get_all_articles_paged() throws Exception {
+        // given
+        int page = 1;
+        int size = 2;
+        // when
+        when(articleService.findAllPaged(page,size))
+                .thenReturn(List.of(articleDtoList.get(2)));
+        String expected = new ObjectMapper().writeValueAsString(new ArrayList<>(List.of(articleDtoList.get(2))));
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.get("/client/articles/pages")
+                        .param("page", String.valueOf(page))
+                        .param("size", String.valueOf(size)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(result -> JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false));
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void get_all_articles_paged_by_category() throws Exception {
+        // given
+        int page = 1;
+        int size = 1;
+        String category = "politics";
+        // when
+        when(articleService.findByCategoryPaged(page,size,category))
+                .thenReturn(new ArrayList<>(List.of(articleDtoList.get(2))));
+        String expected = new ObjectMapper().writeValueAsString(new ArrayList<>(List.of(articleDtoList.get(2))));
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.get("/client/articles/pages")
+                        .param("page", String.valueOf(page))
+                        .param("size", String.valueOf(size))
+                        .param("category", category))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(result -> JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false));
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void get_all_articles_paged_400_bad_request() throws Exception {
+        // given
+        int page = -1;
+        int size = 1;
+        String category = "politics";
+        // when
+        when(articleService.findByCategoryPaged(page,size,category)).thenThrow(IllegalArgumentException.class);
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.get("/client/articles/pages")
+                        .param("page", String.valueOf(page))
+                        .param("size", String.valueOf(size))
+                        .param("category", category))
+                .andExpect(status().isBadRequest());
     }
 }
