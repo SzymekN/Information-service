12ef250d775f9490adb7d8150b85d3cd430c4cd4
Oskar Ożóg <106389146+xDekann@users.noreply.github.com>
2023-04-17 01:22:11 +0200

    Paginated endpoints + initial backend readme (#18)
    * Paginated endpoints + initial backend readme
    
    Added paginated endpoints for client side articles that support both all articles and by category pagination. Also added db and integration tests for them and initial readme for these endpoints.
    
    * Typo fix

--- client/src/main/java/com/client/repository/ArticleRepository.java
+++ client/src/main/java/com/client/repository/ArticleRepository.java
@@ -1,6 +1,8 @@
 package com.client.repository;
 
 import com.client.model.entity.Article;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +18,12 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     @Query("SELECT a FROM Article a LEFT JOIN FETCH a.endorsements e " +
             "JOIN FETCH a.journalist j WHERE a.category = :category")
     List<Article> findByCategory(@Param("category") String category);
+    @Query("SELECT a FROM Article a LEFT JOIN FETCH a.endorsements e " +
+            "JOIN FETCH a.journalist j")
+    Slice<Article> findAllPaged(Pageable pageable);
+    @Query("SELECT a FROM Article a LEFT JOIN FETCH a.endorsements e " +
+            "JOIN FETCH a.journalist j WHERE a.category = :category")
+    Slice<Article> findByCategoryPaged(@Param("category") String category, Pageable pageable);
+
+
 }
