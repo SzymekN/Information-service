2462a42bcd127efe3f08f6158c569f8fb3e438b7
Rafał Matusiak <rmatusiak159@gmail.com>
2023-06-01 21:30:31 +0200

    Returning the logged-in user object - modification

--- client/src/main/java/com/client/service/LoginServiceImpl.java
+++ client/src/main/java/com/client/service/LoginServiceImpl.java
@@ -1,5 +1,6 @@
 package com.client.service;
 
+import com.client.model.dto.AuthenticatedUserDto;
 import com.client.model.dto.LoginDto;
 import com.client.model.dto.UserRegistrationDto;
 import com.client.model.entity.User;
@@ -43,20 +44,35 @@ public class LoginServiceImpl implements LoginService {
     }
 
     @Override
-    public ResponseEntity<String> setUserSession(HttpServletRequest request, HttpServletResponse response, UserRegistrationDto userRegistrationDto, LoginDto loginDto) {
+    public ResponseEntity<Object> setUserSession(HttpServletRequest request, HttpServletResponse response, UserRegistrationDto userRegistrationDto, LoginDto loginDto) {
         Authentication authentication = null;
+        AuthenticatedUserDto authenticatedUserDto = null;
         if (userRegistrationDto != null && GOOGLE_SUPPLIER.equals(userRegistrationDto.getSupplier())) {
             UserDetails user = clientDetailsService.loadUserByUsernameOAuth2(userRegistrationDto);
             authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            User userFromDb = userRepository.findUserByName(userRegistrationDto.getUsername());
+            authenticatedUserDto = userToAuthenticationDto(userFromDb);
         } else if (loginDto != null) {
             User userFromDb = userRepository.findUserByName(loginDto.getUsername());
             if (userFromDb != null && !userFromDb.getEnabled()) return new ResponseEntity<>("Account has not been activated yet!", HttpStatus.FORBIDDEN);
             authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
+            authenticatedUserDto = userToAuthenticationDto(userFromDb);
         }
         SecurityContextHolder.getContext().setAuthentication(authentication);
         basicService.roleCookieCreation(request, response);
         HttpSession session = request.getSession();
         session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
-        return ResponseEntity.ok("Login successful!");
+        return ResponseEntity.ok().body(authenticatedUserDto);
+    }
+
+    public AuthenticatedUserDto userToAuthenticationDto(User user) {
+        return AuthenticatedUserDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .name(user.getUserDetails().getName())
+                .surname(user.getUserDetails().getSurname())
+                .email(user.getUserDetails().getEmail())
+                .supplier(user.getUserDetails().getSupplier())
+                .build();
     }
 }
--- editorial/src/main/java/com/editorial/service/LoginService.java
+++ editorial/src/main/java/com/editorial/service/LoginService.java
@@ -3,7 +3,8 @@ package com.editorial.service;
 import com.editorial.model.dto.LoginDto;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
 
 public interface LoginService {
-    void setUserSession(HttpServletRequest request, HttpServletResponse response, LoginDto loginDto);
+    ResponseEntity<Object> setUserSession(HttpServletRequest request, HttpServletResponse response, LoginDto loginDto);
 }
